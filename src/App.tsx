025a9617daa923e322c4a055df66a2648686602a
mixck20 <deloriamicko@gmail.com>
2025-02-06 13:12:11 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="name">
+      <h2>Jordan Micko S. Deloria</h2>
+      <div className="box">
+        <p>CPEITEL</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
