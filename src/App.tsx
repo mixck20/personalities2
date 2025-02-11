353833a4111fdf19c038cfc2d6ce9b79a59f8fbc
mixck20 <deloriamicko@gmail.com>
2025-02-11 15:36:56 +0800

    initial-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import './App.css';
+import { useState } from 'react';
+import { sculptureList } from './data.tsx';
 
-function App() {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < sculptureList.length - 1;
+  const hasPrevious = index > 0;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleBackClick() {
+    if (hasPrevious) {
+      setIndex(index - 1);
+    } else {
+      setIndex(sculptureList.length - 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="name">
-      <h2>Jordan Micko S. Deloria</h2>
-      <div className="box">
-        <p>CPEITEL</p>
-      </div>
-    </div>
+    <>
+    <h1>Jordan Micko S. Deloria</h1>
+      <button onClick={handleBackClick}>
+        Back
+      </button>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        from {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+    </>
   );
 }
-
-export default App;
---
+++ src/data.tsx
@@ -0,0 +1,74 @@
+export const sculptureList = [{
+    name: 'Michael Kaiser',
+    artist: 'Blue Lock',
+    description: 'Michael Kaiser (ミヒャエル・カイザー Mihyaeru Kaizā?) is a prodigy U-20 forward from Germany who plays for Bastard München during the Neo-Egoist League as the teams ace and main striker. Kaiser is regarded as a genius and is also a member of the New Generation World XI.',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRelB5VpeA5X3YrAHo8hsb8vqvxXMwk5V_TlQ&s',
+    alt: 'Fragile Egoist from Bastard Munchen'
+  }, {
+    name: 'McLovin',
+    artist: 'SuperBad',
+    description: 'McLovin is the self-appointed nickname of a nerdy character in the movie Superbad who gains the respect of his peers by successfully purchasing alcohol with a fake ID that states his name is simply McLovin',
+    url: 'https://i1.sndcdn.com/artworks-GQyoC6yYzY0TOj73-85zTVw-t500x500.jpg',
+    alt: 'Nerd Guy from Superbad'
+  }, {
+    name: 'Scott Pilgrim',
+    artist: 'Scott Pilgrim',
+    description: 'Scott Pilgrim is a slacker musician who is trying to win a competition to get a record deal, while also battling the seven evil exes of his new girlfriend Ramona Flowers, played by Mary Elizabeth Winstead.',
+    url: 'https://img.pastemagazine.com/wp-content/uploads/2022/06/20205849/scottpilgrim_main_edit.jpg',
+    alt: 'Terrible boy'
+  }, {
+    name: 'Isagi Yoichi',
+    artist: 'Blue Lock',
+    description: 'He is a second-year student at Ichinan High School who was a player for the schools football team. After losing a match and arriving at the Blue Lock facility like all the other 300 participants and became a member of Team Z for the First Selection. Hes main goal is to become the worlds best striker.',
+    url: 'https://i.pinimg.com/736x/8b/ac/e0/8bace01a3c415238eefca78d6a75cc39.jpg',
+    alt: 'Blue locks Heart'
+  }, {
+    name: 'Shadow The Hedgehog',
+    artist: 'Sonic 3',
+    description: 'Shadow the Hedgehog is a character created by the Japanese game developers Takashi Iizuka, Shiro Maekawa, and Kazuyuki Hoshino. He is a major character in Segas Sonic the Hedgehog franchise.',
+    url: 'https://www.dexerto.com/cdn-image/wp-content/uploads/2024/12/13/shadow-the-hedgehog-sonic-3-ending.jpg?width=1200&quality=60&format=auto',
+    alt: 'Ultimate life form'
+  }, {
+    name: 'Joe Goldberg',
+    artist: 'YOU',
+    description: 'Joe is a serial killer, stalker and former bookstore manager who, upon meeting Guinevere Beck at his workplace in New York, develops an extreme, toxic and delusional obsession with her.',
+    url: 'https://miro.medium.com/v2/resize:fit:1024/1*04S3MAUGjQVJy-zee3tG7A.jpeg',
+    alt: 'Bro wants'
+  }, {
+    name: 'Lamine Yamal',
+    artist: 'Barcelona FC',
+    description: "Lamine Yamal Nasraoui Ebana is a Spanish professional footballer who plays as a winger for La Liga club Barcelona and the Spain national team.",
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0_Q6oRCNREuAFXIlDpfXl7qWIwicnbs8VFA&s',
+    alt: 'Goat'
+  }, {
+    name: 'Frieren',
+    artist: 'Frieren: Beyond Journeys End',
+    description: "The elven mage Frieren is a former member of a party of adventurers who defeated the Demon King and restored harmony to the world after a ten-year quest.",
+    url: 'https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F5fcd63b9-7a26-4562-99c5-2469df509194_1303x733.jpeg',
+    alt: 'The elven mage'
+  }, {
+    name: 'Wanderer',
+    artist: 'Genshin Impact',
+    description: 'The Wanderer came into existence in place of his previous incarnation after the latter expunged his previous appellations and their respective histories from Irminsul. ',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRb9bBWpo8cfCdnFyv9ZB-LkdmZFBJwPuN4Q&s',
+    alt: 'Scaramouche'
+  }, {
+    name: 'Chinatsu Kano',
+    artist: 'Blue Box',
+    description: 'Chinatsu Kano (鹿野 千夏, Kano Chinatsu?) is one of the main characters of the Blue Box series. She is a third-year student at Eimei Senior High and a member of the basketball team.',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQraTqdz6_n2JhuhzQpepUqcepNC1AjOi_2_Q&s',
+    alt: 'Prodigy Basketball'
+  }, {
+    name: 'Okarun',
+    artist: 'Dandadan',
+    description: 'Ken Takakura ( 高 たか 倉 くら 健 けん , Takakura Ken?), also known as Okarun (オカルン, Okarun?), is the main male protagonist of the manga series Dandadan.',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS29E9fEtxbhiKcjUs35jZ9CJQQZ-Fw25eXg&s',
+    alt: 'Golden Ball Power'
+  }, {
+    name: 'Yatora Yaguchi',
+    artist: 'Blue Period',
+    description: 'Yatora Yaguchi is the main protagonist of the manga and anime series Blue Period. He is a teenager who attends his high school as a second-year student. He has great academic skills regardless of being a troublesome student.',
+    url: 'https://boo-prod.b-cdn.net/database/profiles/16861696666940380bbdb5de7f7725a2b7d8bb01f764c.jpg',
+    alt: 'Prodigy in Art'
+  }];
+  
